6c083cb232579d524b8e580f90463b8bacdf1534
Leviathan143 <Leviathan143@users.noreply.github.com>
2018-10-07 09:53:50 +1300

    Add NonNullList collector

--- src/main/java/daomephsta/umbra/streams/UmbraCollectors.java
+++ src/main/java/daomephsta/umbra/streams/UmbraCollectors.java
@@ -1,17 +1,27 @@
 package daomephsta.umbra.streams;
 
+import java.util.Collection;
 import java.util.stream.Collector;
 
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.NonNullList;
 
 public class UmbraCollectors
 {
-	public static final Collector<NBTBase, ?, NBTTagList> NBT_LIST = Collector.of(NBTTagList::new, NBTTagList::appendTag, 
-		(a, b) ->
+	public static final Collector<NBTBase, ?, NBTTagList> NBT_LIST = Collector.of(NBTTagList::new,
+		NBTTagList::appendTag, (a, b) ->
 		{
 			b.forEach(a::appendTag);
 			return b;
 		});
-}
 
+	public static <I> Collector<I, ?, NonNullList<I>> nonnullList()
+	{
+		return Collector.of(() -> NonNullList.create(), Collection::add, (a, b) ->
+		{
+			a.addAll(b);
+			return a;
+		});
+	}
+}
