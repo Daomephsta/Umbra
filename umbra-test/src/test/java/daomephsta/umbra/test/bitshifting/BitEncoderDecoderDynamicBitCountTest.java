ff7f8da9e967f39bbfbddcba9e5c2885f14680b0
Daomephsta <dtnilfisk@gmail.com>
2018-10-12 20:48:03 +1300

    Update tests to JUnit 5

--- umbra-test/src/test/java/daomephsta/umbra/test/bitshifting/BitEncoderDecoderDynamicBitCountTest.java
+++ umbra-test/src/test/java/daomephsta/umbra/test/bitshifting/BitEncoderDecoderDynamicBitCountTest.java
@@ -1,14 +1,14 @@
 package daomephsta.umbra.test.bitshifting;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.PrimitiveIterator;
 import java.util.stream.IntStream;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import daomephsta.umbra.bitmanipulation.IBitEncoderDecoder;
 
@@ -22,14 +22,14 @@ public class BitEncoderDecoderDynamicBitCountTest
 			IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.dynamicBitCount();
 			bitEncoderDecoder.set(bitEncoderDecoder.size() - 2);
 			bitEncoderDecoder.set(bitEncoderDecoder.size() - 3);
-			assertEquals("decode() failed to correctly decode a whole binary sequence", 6, bitEncoderDecoder.decode());
+			assertEquals(6, bitEncoderDecoder.decode(), "decode() failed to correctly decode a whole binary sequence");
 		}
 		//Partial encode
 		{
 			IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.dynamicBitCount();
 			bitEncoderDecoder.set(0);
 			bitEncoderDecoder.set(1);
-			assertEquals("decode() failed to correctly decode part of a binary sequence", 3, bitEncoderDecoder.decode(0, 2));
+			assertEquals(3, bitEncoderDecoder.decode(0, 2), "decode() failed to correctly decode part of a binary sequence");
 		}
 	}
 
@@ -52,15 +52,14 @@ public class BitEncoderDecoderDynamicBitCountTest
 				&& bitEncoderDecoder.get(bitEncoderDecoder.size() - 2) 
 				&& !bitEncoderDecoder.get(bitEncoderDecoder.size() - 1);
 			
-			assertTrue("encode() failed to correctly encode an integer as a whole binary sequence", correctEncoding);
+			assertTrue(correctEncoding, "encode() failed to correctly encode an integer as a whole binary sequence");
 		}
 		//Partial encode
 		{
 			IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.dynamicBitCount();
 			bitEncoderDecoder.encode(0, 2, 3);
-			assertTrue("encode() failed to correctly encode an integer into part of a binary sequence",
-				bitEncoderDecoder.get(0) && bitEncoderDecoder.get(1) && !bitEncoderDecoder.get(2) 
-				&& !bitEncoderDecoder.get(3));
+			assertTrue(bitEncoderDecoder.get(0) && bitEncoderDecoder.get(1) && !bitEncoderDecoder.get(2) && !bitEncoderDecoder.get(3), 
+				"encode() failed to correctly encode an integer into part of a binary sequence");
 		}
 	}
 
@@ -74,16 +73,18 @@ public class BitEncoderDecoderDynamicBitCountTest
 			{
 				IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.dynamicBitCount();
 				bitEncoderDecoder.encode(testInt);
-				assertEquals(String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as a whole binary sequence.", 
-					bitEncoderDecoder.getClass().getSimpleName(), testInt), testInt, bitEncoderDecoder.decode());
+				assertEquals(testInt, bitEncoderDecoder.decode(), 
+					String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as a whole binary sequence.", 
+					bitEncoderDecoder.getClass().getSimpleName(), testInt));
 			}
 			//Partial encode
 			{
 				int FROM = 2, TO = 6;
 				IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.dynamicBitCount();
 				bitEncoderDecoder.encode(FROM, TO, testInt);
-				assertEquals(String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as part of a binary sequence.", 
-					bitEncoderDecoder.getClass().getSimpleName(), testInt), testInt, bitEncoderDecoder.decode(FROM, TO));
+				assertEquals(testInt, bitEncoderDecoder.decode(FROM, TO), 
+					String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as part of a binary sequence.", 
+					bitEncoderDecoder.getClass().getSimpleName(), testInt));
 			}
 		}
 	}
@@ -102,8 +103,9 @@ public class BitEncoderDecoderDynamicBitCountTest
 			actual[i] = iter.nextInt();
 		}
 		int[] expected = {1, 0, 0, 1};
-		assertArrayEquals(String.format("The iterator %s did not produce the expected integers %s", 
-			iter.getClass().getSimpleName(), Arrays.toString(expected)), expected, actual);
+		assertArrayEquals(expected, actual, 
+			String.format("The iterator %s did not produce the expected integers %s", 
+			iter.getClass().getSimpleName(), Arrays.toString(expected)));
 		
 	}
 }
--- umbra-test/src/test/java/daomephsta/umbra/test/bitshifting/BitEncoderDecoderFixedBitCountTest.java
+++ umbra-test/src/test/java/daomephsta/umbra/test/bitshifting/BitEncoderDecoderFixedBitCountTest.java
@@ -1,14 +1,14 @@
 package daomephsta.umbra.test.bitshifting;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.PrimitiveIterator;
 import java.util.stream.IntStream;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import daomephsta.umbra.bitmanipulation.IBitEncoderDecoder;
 
@@ -22,14 +22,14 @@ public class BitEncoderDecoderFixedBitCountTest
 			IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.fixedBitCount(4);
 			bitEncoderDecoder.set(1);
 			bitEncoderDecoder.set(2);
-			assertEquals("decode() failed to correctly decode a whole binary sequence", 6, bitEncoderDecoder.decode());
+			assertEquals(6, bitEncoderDecoder.decode(), "decode() failed to correctly decode a whole binary sequence");
 		}
 		//Partial encode
 		{
 			IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.fixedBitCount(4);
 			bitEncoderDecoder.set(0);
 			bitEncoderDecoder.set(1);
-			assertEquals("decode() failed to correctly decode part of binary sequence", 3, bitEncoderDecoder.decode(0, 2));
+			assertEquals(3, bitEncoderDecoder.decode(0, 2), "decode() failed to correctly decode part of binary sequence");
 		}
 	}
 
@@ -40,17 +40,15 @@ public class BitEncoderDecoderFixedBitCountTest
 		{
 			IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.fixedBitCount(4);
 			bitEncoderDecoder.encode(6);
-			assertTrue("encode() failed to correctly encode an integer as a whole binary sequence", 
-				!bitEncoderDecoder.get(0) && bitEncoderDecoder.get(1) && bitEncoderDecoder.get(2) 
-				&& !bitEncoderDecoder.get(3));
+			assertTrue(!bitEncoderDecoder.get(0) && bitEncoderDecoder.get(1) && bitEncoderDecoder.get(2) && !bitEncoderDecoder.get(3), 
+				"encode() failed to correctly encode an integer as a whole binary sequence");
 		}
 		//Partial encode
 		{
 			IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.fixedBitCount(4);
 			bitEncoderDecoder.encode(0, 2, 3);
-			assertTrue("encode() failed to correctly encode an integer into part of a binary sequence",
-				bitEncoderDecoder.get(0) && bitEncoderDecoder.get(1) && !bitEncoderDecoder.get(2) 
-				&& !bitEncoderDecoder.get(3));
+			assertTrue(bitEncoderDecoder.get(0) && bitEncoderDecoder.get(1) && !bitEncoderDecoder.get(2) && !bitEncoderDecoder.get(3),
+				"encode() failed to correctly encode an integer into part of a binary sequence");
 		}
 	}
 
@@ -64,16 +62,18 @@ public class BitEncoderDecoderFixedBitCountTest
 			{
 				IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.fixedBitCount(4);
 				bitEncoderDecoder.encode(testInt);
-				assertEquals(String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as a whole binary sequence.", 
-					bitEncoderDecoder.getClass().getSimpleName(), testInt), testInt, bitEncoderDecoder.decode());
+				assertEquals(testInt, bitEncoderDecoder.decode(), 
+					String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as a whole binary sequence.", 
+					bitEncoderDecoder.getClass().getSimpleName(), testInt));
 			}
 			//Partial encode
 			{
 				int FROM = 2, TO = 6;
 				IBitEncoderDecoder bitEncoderDecoder = IBitEncoderDecoder.fixedBitCount(8);
 				bitEncoderDecoder.encode(FROM, TO, testInt);
-				assertEquals(String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as part of a binary sequence.", 
-					bitEncoderDecoder.getClass().getSimpleName(), testInt), testInt, bitEncoderDecoder.decode(FROM, TO));
+				assertEquals(testInt, bitEncoderDecoder.decode(FROM, TO),
+					String.format("%1$s#encode() & %1$s#encode() do not map 1:1 for value %2$s when encoding as part of a binary sequence.", 
+					bitEncoderDecoder.getClass().getSimpleName(), testInt));
 			}
 		}
 	}
@@ -92,8 +92,9 @@ public class BitEncoderDecoderFixedBitCountTest
 			actual[i] = iter.nextInt();
 		}
 		int[] expected = {1, 0, 0, 1};
-		assertArrayEquals(String.format("The iterator %s did not produce the expected integers %s", 
-			iter.getClass().getSimpleName(), Arrays.toString(expected)), expected, actual);
+		assertArrayEquals(expected, actual, 
+			String.format("The iterator %s did not produce the expected integers %s", 
+			iter.getClass().getSimpleName(), Arrays.toString(expected)));
 		
 	}
 }
