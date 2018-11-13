61b25d1a45874d927b7159a04aa7728140e35aa5
Daomephsta <dtnilfisk@gmail.com>
2018-11-13 19:33:07 +1300

    Add IBlockState serialisation & deserialisation methods to NBTExtensions

--- umbra-minecraft/src/main/java/daomephsta/umbra/nbt/NBTExtensions.java
+++ umbra-minecraft/src/main/java/daomephsta/umbra/nbt/NBTExtensions.java
@@ -1,6 +1,14 @@
 package daomephsta.umbra.nbt;
 
+import java.util.Map.Entry;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class NBTExtensions
 {
@@ -19,5 +27,47 @@ public class NBTExtensions
 	public static <E extends Enum<E>> void setEnumConstant(NBTTagCompound nbt, String key, E enumConstant)
 	{
 		nbt.setString(key, enumConstant.name());
+	}
+	
+	public static IBlockState getBlockState(NBTTagCompound nbt, String key)
+	{
+		NBTTagCompound stateTag = nbt.getCompoundTag(key);
+		Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(stateTag.getString("block")));
+		NBTTagCompound propertiesTag = stateTag.getCompoundTag("properties");
+		BlockStateContainer stateContainer = block.getBlockState();
+		IBlockState state = block.getDefaultState();
+		for (String propName : propertiesTag.getKeySet())
+		{
+			IProperty<?> property = stateContainer.getProperty(propName);
+			state = deserialisePropertyValue(state, property, propertiesTag.getString(propName));
+		}
+		return state;
+	}
+	
+	private static <T extends Comparable<T>> IBlockState deserialisePropertyValue(IBlockState state, IProperty<T> property, String serValue)
+	{
+		T value = property.parseValue(serValue).get();
+		return state.withProperty(property, value);
+	}
+	
+	public static void setBlockState(NBTTagCompound nbt, String key, IBlockState state)
+	{
+		NBTTagCompound stateTag = new NBTTagCompound();
+		stateTag.setString("block", state.getBlock().getRegistryName().toString());
+		NBTTagCompound propertiesTag = new NBTTagCompound();
+		for (Entry<IProperty<?>, Comparable<?>> entry : state.getProperties().entrySet())
+		{
+			IProperty<?> property = entry.getKey();
+			propertiesTag.setString(property.getName(), serialisePropertyValue(property, entry.getValue()));
+		}
+		stateTag.setTag("properties", propertiesTag);
+		
+		nbt.setTag(key, stateTag);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static <T extends Comparable<T>> String serialisePropertyValue(IProperty<T> property, Comparable<?> value)
+	{
+		return property.getName((T) value);
 	}
 }
