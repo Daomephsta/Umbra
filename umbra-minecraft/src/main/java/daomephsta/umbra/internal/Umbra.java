d8abb7397389ed75c42fc6c5885e845809860f52
Daomephsta <dtnilfisk@gmail.com>
2018-11-15 20:42:31 +1300

    Make Umbra a mod in its own right

---
+++ umbra-minecraft/src/main/java/daomephsta/umbra/internal/Umbra.java
@@ -0,0 +1,14 @@
+package daomephsta.umbra.internal;
+
+import daomephsta.umbra.scheduler.ActionScheduler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Umbra.MODID, name = Umbra.MODNAME, version = Umbra.VERSION)
+public class Umbra
+{
+	public static final String MODNAME = "Umbra";
+	public static final String MODID = "umbra";
+	public static final String VERSION = "0.0.1";
+}
