a7f9bb6d2528ceb498e33a9b837a42d15b34211a
Daomephsta <dtnilfisk@gmail.com>
2018-11-20 15:56:26 +1300

    Make changes to collectors, suggested by Kit

--- umbra-minecraft/src/main/java/daomephsta/umbra/streams/UmbraCollectors.java
+++ umbra-minecraft/src/main/java/daomephsta/umbra/streams/UmbraCollectors.java
@@ -1,6 +1,7 @@
 package daomephsta.umbra.streams;
 
 import java.util.Collection;
+import java.util.function.BiConsumer;
 import java.util.stream.Collector;
 
 import net.minecraft.nbt.NBTBase;
@@ -9,16 +10,29 @@ import net.minecraft.util.NonNullList;
 
 public class UmbraCollectors
 {
-	public static final Collector<NBTBase, ?, NBTTagList> NBT_LIST = Collector.of(NBTTagList::new,
-		NBTTagList::appendTag, (a, b) ->
-		{
-			b.forEach(a::appendTag);
-			return b;
-		});
-
-	public static <I> Collector<I, ?, NonNullList<I>> nonnullList()
+	public static <T extends NBTBase> Collector<T, ?, NBTTagList> toNBTList(Class<T> expectedClass)
 	{
-		return Collector.of(() -> NonNullList.create(), Collection::add, (a, b) ->
+		return Collector.of(NBTTagList::new,
+			throwingAccumulator(expectedClass), (a, b) ->
+			{
+				b.forEach(a::appendTag);
+				return b;
+			});
+	}
+	
+	private static <T extends NBTBase> BiConsumer<NBTTagList, T> throwingAccumulator(Class<T> expectedClass)
+	{
+		return (list, element) -> 
+		{
+			if (!expectedClass.isInstance(element))
+				throw new IllegalStateException(String.format("%s was an instance of %s, not the expected type %s", element, element.getClass(), expectedClass));
+			list.appendTag(element);
+		};
+	}
+ 
+	public static <I> Collector<I, ?, NonNullList<I>> toNonNullList()
+	{
+		return Collector.of(NonNullList::create, Collection::add, (a, b) ->
 		{
 			a.addAll(b);
 			return a;
